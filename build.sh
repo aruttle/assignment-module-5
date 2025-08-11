bed606792cc754b0b95ae0d3563dd96bb110a528
aruttle <alan.ruttle@analog.com>
2025-08-11 15:21:14 +0100

    Added build.sh

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Exit immediately if a command exits with a non-zero status
+set -o errexit  
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Run migrations
+python manage.py migrate
+
+# Collect static files without prompting
+python manage.py collectstatic --noinput
