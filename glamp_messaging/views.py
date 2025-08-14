4cedc8914bd0cd8eba232110224c51f854b04b22
aruttle <alan.ruttle@analog.com>
2025-08-14 10:44:57 +0100

    Fix for messages vs messages collisions

--- glamp_messaging/views.py
+++ glamp_messaging/views.py
@@ -1,34 +1,29 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-from django.contrib import messages as django_messages
 from .models import Message
 from .forms import MessageForm
 
-
 @login_required
 def inbox(request):
-    items = (
+    inbox_qs = (
         Message.objects
-        .filter(recipient=request.user)
         .select_related('sender', 'recipient')
+        .filter(recipient=request.user)
         .order_by('-sent_at')
     )
-    return render(request, 'glamp_messaging/inbox.html', {'items': items})
-
+    return render(request, 'glamp_messaging/inbox.html', {
+        'inbox_messages': inbox_qs,   # <-- renamed
+    })
 
 @login_required
 def view_message(request, message_id):
-    message_obj = get_object_or_404(
-        Message.objects.select_related('sender', 'recipient'),
-        id=message_id,
-        recipient=request.user
-    )
+    message_obj = get_object_or_404(Message, id=message_id, recipient=request.user)
     if not message_obj.read:
         message_obj.read = True
         message_obj.save(update_fields=['read'])
-
-    return render(request, 'glamp_messaging/view_message.html', {'message_obj': message_obj})
-
+    return render(request, 'glamp_messaging/view_message.html', {
+        'message_obj': message_obj,
+    })
 
 @login_required
 def send_message(request):
@@ -38,7 +33,6 @@ def send_message(request):
             msg = form.save(commit=False)
             msg.sender = request.user
             msg.save()
-            django_messages.success(request, 'Message sent.')
             return redirect('glamp_messaging:inbox')
     else:
         form = MessageForm()
