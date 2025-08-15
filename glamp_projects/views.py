cefdcf1d4c1e88140ec5a89b8f48fb89d69afdee
aruttle <alan.ruttle@analog.com>
2025-08-15 11:06:12 +0100

    Added functionality to Projects (internal view)

--- glamp_projects/views.py
+++ glamp_projects/views.py
@@ -1,4 +1,15 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 
+
+@login_required
 def project_list(request):
+    """
+    Staff/superusers only. Non-staff get a friendly message and are sent home.
+    """
+    if not (request.user.is_staff or request.user.is_superuser):
+        messages.error(request, "Projects is a staff-only area.")
+        return redirect('core:home')
+
     return render(request, 'glamp_projects/project_list.html')
